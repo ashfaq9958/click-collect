78fab2561dc85f103b441f5b9f2e2479abc6383d
ashfaq9958 <137767028+ashfaq9958@users.noreply.github.com>
2024-06-08 12:56:17 +0530

    working on login page

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import companylogo from "../../assets/elogo2.jpg";
 import google from "../../assets/google.png";
 import facebook from "../../assets/facebook.png";
+import login from "../../assets/img_4.jpg";
 const SignIn = () => {
   return (
-    <div className="flex w-full h-screen ">
+    <div className="flex w-full h-screen">
       <div className=" w-1/2 ">
         <div className="flex justify-center">
           <img src={companylogo} alt="Company Logo" className="h-28 mt-2" />
@@ -14,7 +15,7 @@ const SignIn = () => {
             <h1 className="text-[46px] text-[#ff5e38] font-semibold font-eczar inline  mr-44">
               Hello,
             </h1>
-            <p className="text-[46px] font-semibold font-eczar -mt-4">
+            <p className="text-[46px] font-bold font-eczar -mt-4 ">
               Welcome Back !!
             </p>
           </div>
@@ -24,7 +25,7 @@ const SignIn = () => {
                 for="email"
                 className="block text-sm font-medium font-eczar  "
               >
-                Email address
+                Email address *
               </label>
               <input
                 type="email"
@@ -39,7 +40,7 @@ const SignIn = () => {
                 for="password"
                 className="block text-sm font-medium  font-eczar"
               >
-                Password
+                Password *
               </label>
               <input
                 type="password"
@@ -95,7 +96,7 @@ const SignIn = () => {
                 className=" w-full inline-flex justify-center gap-3 !font-eczar items-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium  hover:bg-gray-50"
               >
                 <img className="h-5 w-5 " src={google} alt="Google" />
-                <span >Google</span>
+                <span>Google</span>
               </a>
             </div>
 
@@ -105,11 +106,14 @@ const SignIn = () => {
                 className="w-full inline-flex justify-center gap-3 !font-eczar items-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium  hover:bg-gray-50"
               >
                 <img className="h-6 w-6" src={facebook} alt="Google" />
-                <span >Facebook</span>
+                <span>Facebook</span>
               </a>
             </div>
           </div>
         </div>
+      </div>
+      <div className="">
+        <img src={login} alt="Login Image" className="w-full h-screen" />
       </div>
     </div>
   );
