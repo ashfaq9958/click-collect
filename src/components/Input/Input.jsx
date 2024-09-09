a38c96a0a25f8817e54c5d0c58d04cadf50c4024
ashfaq9958 <137767028+ashfaq9958@users.noreply.github.com>
2024-09-10 00:34:15 +0530

    changes on login and write logic of login

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 
-const Input = ({ label, type, name }) => {
+const Input = ({ label, type, name, error, ...props }) => {
   const [isVisible, setVisible] = useState(false);
 
   const toggleVisibility = () => {
@@ -19,8 +19,9 @@ const Input = ({ label, type, name }) => {
         type={typeInput}
         id={name}
         name={name}
-        required
-        className="mt-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm font-eczar"
+        {...props}
+        className={`mt-1 w-full px-3 py-2 border ${error ? "border-red-500" : "border-gray-300"
+          } rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
       />
       {type === "password" && (
         <span
@@ -34,8 +35,10 @@ const Input = ({ label, type, name }) => {
           )}
         </span>
       )}
+      {error && <span className="text-red-600 text-sm mt-1">{error}</span>}
     </div>
   );
 };
 
 export default Input;
+  
--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -6,104 +6,141 @@ import login from "../../assets/img_3.jpg";
 import Input from "../Input/Input";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
+import { Formik, useFormik } from "formik";
+import * as Yup from "yup"; // For validation
+
+// Validation schema using Yup
+const validationSchema = Yup.object({
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Email is required"),
+  password: Yup.string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
+});
 
 const SignIn = () => {
   const navigate = useNavigate();
-  const handleNavigation = () => {
-    navigate("/");
-    toast.success("Login successful. Welcome back!", { duration: 3000 });
+
+  // Submit handler
+  const handleSubmit = (values, { setSubmitting }) => {
+    setTimeout(() => {
+      // Mock login logic
+      toast.success("Login successful. Welcome back!", { duration: 3000 });
+      navigate("/");
+      setSubmitting(false);
+    }, 1000); // Simulating a login request delay
   };
+
   return (
-    <div className="flex md:justify-between  md:flex-row w-full h-screen bg-gradient-to-r from-gray-100 to-gray-200">
-      <div className="hidden lg:block w-1/2">
-        <img src={login} alt="login" className="h-screen w-full object-cover" />
-      </div>
-      <div className="w-full md:w-[50%] lg:w-[35%] mx-auto flex flex-col p-6 md:p-10 lg:p-12">
-        <div className="flex justify-center mb-6">
-          <img
-            src={companylogo}
-            alt="Company Logo"
-            className="h-16 md:h-20 rounded-full shadow-lg"
-          />
-        </div>
-        <div className="text-center mb-6">
-          <h1 className="text-3xl md:text-4xl text-[#ff5e38] font-bold">
-            Welcome Back!
-          </h1>
-          <p className="text-lg md:text-xl text-gray-500">
-            Sign in to continue
-          </p>
-        </div>
-        <form className="space-y-6">
-          <div>
-            <Input
-              label="Email address"
-              type="email"
-              name="email"
-              className="rounded-md border-gray-300 shadow-sm focus:border-[#FF5E38] focus:ring-[#FF5E38]"
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      {({ handleSubmit, getFieldProps, touched, errors, isSubmitting }) => (
+        <div className="flex md:justify-between  md:flex-row w-full h-screen bg-gradient-to-r from-gray-100 to-gray-200">
+          <div className="hidden lg:block w-1/2">
+            <img
+              src={login}
+              alt="login"
+              className="h-screen w-full object-cover"
             />
           </div>
-          <div>
-            <Input
-              label="Password"
-              type="password"
-              name="password"
-              className="rounded-md border-gray-300 shadow-sm focus:border-[#FF5E38] focus:ring-[#FF5E38]"
-            />
-          </div>
-          <div className="flex justify-between text-sm text-gray-500">
-            <label className="flex items-center space-x-2">
-              <input
-                id="remember_me"
-                name="remember_me"
-                type="checkbox"
-                className="h-4 w-4 text-[#FF5E38] focus:ring-[#FF5E38] border-gray-300 rounded"
+          <div className="w-full md:w-[50%] lg:w-[35%] mx-auto flex flex-col p-6 md:p-10 lg:p-12">
+            <div className="flex justify-center mb-6">
+              <img
+                src={companylogo}
+                alt="Company Logo"
+                className="h-16 md:h-20 rounded-full shadow-lg"
               />
-              <span>Remember me</span>
-            </label>
-            <a href="#" className="text-[#FF5E38] hover:underline">
-              Forgot password?
-            </a>
+            </div>
+            <div className="text-center mb-6">
+              <h1 className="text-3xl md:text-4xl text-[#ff5e38] font-bold">
+                Welcome Back!
+              </h1>
+              <p className="text-lg md:text-xl text-gray-500">
+                Sign in to continue
+              </p>
+            </div>
+            <form className="space-y-6" onSubmit={handleSubmit}>
+              <div>
+                <Input
+                  label="Email address"
+                  type="email"
+                  name="email"
+                  {...getFieldProps("email")}
+                  error={touched.email && errors.email} // Handle error display
+                  className="rounded-md border-gray-300 shadow-sm focus:border-[#FF5E38] focus:ring-[#FF5E38]"
+                />
+              </div>
+              <div>
+                <Input
+                  label="Password"
+                  type="password"
+                  name="password"
+                  {...getFieldProps("password")}
+                  error={touched.password && errors.password} // Handle error display
+                  className="rounded-md border-gray-300 shadow-sm focus:border-[#FF5E38] focus:ring-[#FF5E38]"
+                />
+              </div>
+              <div className="flex justify-between text-sm text-gray-500">
+                <label className="flex items-center space-x-2">
+                  <input
+                    id="remember_me"
+                    name="remember_me"
+                    type="checkbox"
+                    className="h-4 w-4 text-[#FF5E38] focus:ring-[#FF5E38] border-gray-300 rounded"
+                  />
+                  <span>Remember me</span>
+                </label>
+                <a href="#" className="text-[#FF5E38] hover:underline">
+                  Forgot password?
+                </a>
+              </div>
+              <div>
+              <button
+                  type="submit"
+                  disabled={isSubmitting}
+                  className="w-full py-2 px-4 border border-transparent rounded-md shadow-md text-sm font-medium text-white bg-gradient-to-r from-[#FF5E38] to-[#ff7e60] hover:bg-black transition-all focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#FF5E38]"
+                >
+                  {isSubmitting ? "Logging in..." : "Login"}
+                </button>
+              </div>
+            </form>
+            <p className="mt-4 text-center text-sm text-gray-500">
+              Don't have an account?{" "}
+              <a href="#" className="text-blue-600 hover:underline">
+                Sign up
+              </a>
+            </p>
+            <div className="flex items-center my-6">
+              <div className="flex-grow border-t border-gray-300"></div>
+              <span className="mx-4 text-sm text-gray-500">
+                Or continue with
+              </span>
+              <div className="flex-grow border-t border-gray-300"></div>
+            </div>
+            <div className="flex justify-between">
+              <a
+                href="#"
+                className="w-[45%] inline-flex justify-center items-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-700 hover:bg-gray-100 transition-all"
+              >
+                <img className="h-5 w-5 mr-2" src={google} alt="Google" />
+                Google
+              </a>
+              <a
+                href="#"
+                className="w-[45%] inline-flex justify-center items-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-700 hover:bg-gray-100 transition-all"
+              >
+                <img className="h-6 w-6 mr-2" src={facebook} alt="Facebook" />
+                Facebook
+              </a>
+            </div>
           </div>
-          <div>
-            <button
-              onClick={handleNavigation}
-              type="submit"
-              className="w-full py-2 px-4 border border-transparent rounded-md shadow-md text-sm font-medium text-white bg-gradient-to-r from-[#FF5E38] to-[#ff7e60] hover:bg-black transition-all focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#FF5E38]"
-            >
-              Login
-            </button>
-          </div>
-        </form>
-        <p className="mt-4 text-center text-sm text-gray-500">
-          Don't have an account?{" "}
-          <a href="#" className="text-blue-600 hover:underline">
-            Sign up
-          </a>
-        </p>
-        <div className="flex items-center my-6">
-          <div className="flex-grow border-t border-gray-300"></div>
-          <span className="mx-4 text-sm text-gray-500">Or continue with</span>
-          <div className="flex-grow border-t border-gray-300"></div>
         </div>
-        <div className="flex justify-between">
-          <a
-            href="#"
-            className="w-[45%] inline-flex justify-center items-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-700 hover:bg-gray-100 transition-all"
-          >
-            <img className="h-5 w-5 mr-2" src={google} alt="Google" />
-            Google
-          </a>
-          <a
-            href="#"
-            className="w-[45%] inline-flex justify-center items-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-700 hover:bg-gray-100 transition-all"
-          >
-            <img className="h-6 w-6 mr-2" src={facebook} alt="Facebook" />
-            Facebook
-          </a>
-        </div>
-      </div>
-    </div>
+      )}
+    </Formik>
   );
 };
 
